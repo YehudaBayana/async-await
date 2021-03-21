affc5a3f9da818cab5607acf34e609b5efb5f2a9
YehudaBayana <bayanayehuda@gmail.com>
2021-03-21 13:40:42 +0200

    updates

--- main.js
+++ main.js
@@ -1,62 +1,51 @@
 let loadingGif = document.getElementById('imgId');
+// צרו תוכנית שמקבלת מהמשתמש, שם סדרה ומציגה את המידע על המסך.
+// צרו תצוגה של קלט וכפתור, לחיצה על הכפתור מביאה מידע ה API
+//  לאחר לחיצה מוצג על המסך המידע אודות אותן סדרות שקיבלנו מה API
+// לכל סדרה יוצגו שם סדרה, תמונה בינונית, ז'אנרים ולינק לאתר הסדרה.
+// במידה ולא נמצא אובייקט מתאים יש להחזיר הודעה מתאימה.
+// אתגר – בצעו את החיפוש בזמן הקלדה והציגו על המסך את כל התוצאות המתאימות.
 
-function inputAndPrint() {
-  h2Id.innerHTML += inputId.value;
-}
+const moviesApi = 'https://api.tvmaze.com/search/shows?q=';
 
-let clients = [
-  {
-    userName: 'yudaBayana',
-    email: 'yudaBayana@gmail.com',
-    fullName: 'Yuda Bayana',
-    userImg:
-      'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8cGVvcGxlfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-  },
-  {
-    userName: 'daniMangi',
-    email: 'dani@gmail.com',
-    fullName: 'Dain Mangi Bayana',
-    userImg:
-      'https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fHBlb3BsZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-  },
-  {
-    userName: 'yosi',
-    email: 'yosi@gmail.com',
-    fullName: 'yosi cohen',
-    userImg:
-      'https://images.unsplash.com/photo-1463453091185-61582044d556?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fHBlb3BsZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-  },
-];
-
-function myPromise() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      for (let i = 0; i < clients.length; i++) {
-        if (clients[i].userName == inputId.value) {
-          let allProperties = `
-          <div class="card" style="width: 18rem">
-          <div class="card-body">     
-            <img class="card-img-top" src="${clients[i].userImg}"/>
-            <h5 class="card-title"> שם משתמש:  ${clients[i].userName}</h5>
-            <p class="card-text">שם מלא:  ${clients[i].fullName}</p>
-            <a href="#" class="btn btn-primary">אימייל:  ${clients[i].email}</a>
-          </div>
-          </div>`;
-          resolve(allProperties);
-        }
-      }
-      reject(`the username ${inputId.value} is not included`);
-    }, 2000);
+function myPromise(searchValue) {
+  return fetch(`${moviesApi}${searchValue}`, {}).then((res) => {
+    return res.json();
   });
 }
-
-async function inputAndPrint() {
+let allProperties;
+let results;
+async function searchUsers() {
   try {
     loadingGif.src =
       'https://media2.giphy.com/media/3oEjI6SIIHBdRxXI40/200.gif';
-    mainDiv.innerHTML = await myPromise();
-  } catch (rej) {
-    mainDiv.innerHTML = `<h1>${rej}</h1>`;
+    results = await myPromise(searchInput.value);
+    console.log(results);
+    allProperties = '';
+    results.forEach((objItem) => {
+      allProperties += `
+      <div class="card" style="width: 18rem">
+      <div class="card-body">
+        ${
+          objItem.show.image
+            ? `<img class="card-img-top" src="${objItem.show.image.medium}"/>`
+            : '<h1>אין תמונה</h1>'
+        }
+        <h5 class="card-title"> שם הסדרה: ${objItem.show.name} </h5>
+        <p class="card-text">ז'אנר: ${
+          objItem.show.genres[0] ? objItem.show.genres[0] : `לא צוין`
+        }</p>
+        <a href="${objItem.show.url}" class="btn btn-primary">לחץ כאן לצפייה</a>
+      </div>
+      </div>`;
+    });
+    document.getElementById('mainDiv').innerHTML = allProperties;
+    if (results.length == 0) {
+      document.getElementById(
+        'mainDiv'
+      ).innerHTML = `<h1>הסדרה לא נמצאת במאגר</h1>`;
+    }
+  } catch {
   } finally {
     loadingGif.src = '';
   }
